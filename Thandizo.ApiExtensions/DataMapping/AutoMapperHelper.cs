4a4de2ede511e4e54f77a4d5777b4a4f45092ed1
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-03-27 15:44:25 +0200

    added AutoMapper package and AutoMapperHelper for managing an object and collection

---
+++ Thandizo.ApiExtensions/DataMapping/AutoMapperHelper.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using System.Collections.Generic;
+
+namespace Thandizo.ApiExtensions.DataMapping
+{
+    public class AutoMapperHelper<TSource, TDestination>
+    {
+        public TDestination MapToObject(TSource source)
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<TSource, TDestination>();
+            });
+            return config.CreateMapper().Map<TSource, TDestination>(source);
+        }
+
+        public IEnumerable<TDestination> MapToList(IEnumerable<TSource> sourceList)
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<TSource, TDestination>();
+            });
+            var mapper = config.CreateMapper();
+            return mapper.Map<IEnumerable<TSource>, IEnumerable<TDestination>>(sourceList);
+        }
+    }
+}
