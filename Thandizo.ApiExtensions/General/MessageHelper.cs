9bc0cfdb461bde6288a39d998bd19a0003605f98
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-03-27 15:07:58 +0200

    added MessageHelper to hold common errors sent to the called application

---
+++ Thandizo.ApiExtensions/General/MessageHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace Thandizo.ApiExtensions.General
+{
+    public static class MessageHelper
+    {
+        public const string AddNewError = "An error occurred while adding new record";
+        public const string AddNewSuccess = "The record has been created successfully";
+        public const string UpdateError = "An error occurred while updating a record";
+        public const string UpdateSuccess = "The record has been updated successfully";
+        public const string DeleteError = "An error occurred while deleting a record";
+        public const string DeleteSuccess = "The record has been deleted successfully";
+        public const string GetItemError = "An error occurred while retrieving a record";
+        public const string GetListError = "An error occurred while retrieving records";
+    }
+}
