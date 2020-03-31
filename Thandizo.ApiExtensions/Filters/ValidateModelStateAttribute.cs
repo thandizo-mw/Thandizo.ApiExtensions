9d730ca09e99267738b9038afa08557301c7cfa4
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-03-31 14:54:24 +0200

    added ValidateModelStateAttribute for providing cleaner error messages as opposed to default one in Web API

---
+++ Thandizo.ApiExtensions/Filters/ValidateModelStateAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Linq;
+
+namespace Thandizo.ApiExtensions.Filters
+{
+    public class ValidateModelStateAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var errors = string.Join("; ", context.ModelState.Values.SelectMany(v => v.Errors)
+                        .Select(e => e.ErrorMessage));
+
+                context.Result = new BadRequestObjectResult(errors);
+            }
+        }
+    }
+}
